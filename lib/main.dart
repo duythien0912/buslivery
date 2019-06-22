965dbc888c906c952418a792b5c6e6d7da82a4e7
key46 <key46@key46s-MacBook-Air.local>
2019-06-22 18:43:36 +0700

    [Thien] add page

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,142 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import './presentation/custom_icons_icons.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+
+  if (Platform.isAndroid) {
+    SystemUiOverlayStyle systemUiOverlayStyle =
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
+}
 
 class MyApp extends StatelessWidget {
+  static const Map<TargetPlatform, PageTransitionsBuilder> _defaultBuilders =
+      <TargetPlatform, PageTransitionsBuilder>{
+    TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+    TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+  };
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
+        pageTransitionsTheme: PageTransitionsTheme(builders: _defaultBuilders),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  MyHomePage({Key key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomePage(),
+    OrderPage(),
+    UserPage(),
+  ];
 
-  void _incrementCounter() {
+  void onTabTapped(int index) {
     setState(() {
-      _counter++;
+      _currentIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
+      // appBar: AppBar(
+      //   title: Text("Pobus"),
+      // ),
+      body: _children[_currentIndex],
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {},
         child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Container(height: 0.0),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt),
+            title: Container(height: 0.0),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Container(height: 0.0),
+          ),
+          BottomNavigationBarItem(
+            icon: Container(),
+            title: Container(height: 0.0),
+          ),
+        ],
+      ),
+
+      // Container(
+      //   child: Center(child: Text("Alo alo đây là thông báo"),),
+      //   // color: Color.fromRGBO(200, 200, 200, 1),
+      //   height: 45,
+      // ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("HomePage"),
+      ),
+    );
+  }
+}
+
+class OrderPage extends StatelessWidget {
+  const OrderPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("OrderPage"),
+      ),
+    );
+  }
+}
+
+class UserPage extends StatelessWidget {
+  const UserPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("UserPage"),
       ),
     );
   }
